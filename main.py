50265ffb5d91837c6500083ea863b1889155ff2f
UserCyk <flashboy2017@163.com>
2025-02-23 13:13:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""
+@File        :   main.py
+@Time        :   2025/02/23 13:07:24
+@Author      :   Usercyk
+@Description :   test
+"""
+
+if __name__ == "__main__":
+    print("Hello World")
